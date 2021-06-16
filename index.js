5ed192bae9641efe9431ba30afd6a1f25a1d6c04
Moumen Hallak <moumen95@gmail.com>
2021-06-16 22:53:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
-  //add static methods here
+   static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+
+  static titleize(string) {
+    const noCap = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let title = '';
+
+    let titles = string.split(' ').map((word, index) => {
+      if(noCap.includes(word) && index != 0)
+        return word;
+      return this.capitalize(word);
+    })
+
+    return titles.join(' ');
+  }
 }
+
